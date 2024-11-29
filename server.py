60e29653b066d8878c7b5b424d9089c23218008c
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-11-29 10:56:57 +0400

    add DELETE to reset game state after game end

--- server.py
+++ server.py
@@ -9,15 +9,13 @@ CURRENT_TURN = ""
 
 @app.route('/')
 def hello_world():
-    global PLAYER_SHAPE 
-    global BOT_SHAPE 
+    global PLAYER_SHAPE
+    global BOT_SHAPE
     global CURRENT_TURN
-    PLAYER_SHAPE = ""
-    BOT_SHAPE = ""
-    CURRENT_TURN = ""
+    
     return render_template('index.html')
 
-@app.route('/shape', methods=['GET','POST'])
+@app.route('/shape', methods=['GET','POST','DELETE'])
 def shape():
     global PLAYER_SHAPE
     global BOT_SHAPE
@@ -33,9 +31,17 @@ def shape():
         else:
             BOT_SHAPE = "X"
             CURRENT_TURN = "BOT"
-        
+        return jsonify({"player_shape": PLAYER_SHAPE, "bot_shape": BOT_SHAPE, "current_turn": CURRENT_TURN})
+    
     if request.method == 'GET':
         return jsonify({"player_shape": PLAYER_SHAPE, "bot_shape": BOT_SHAPE, "current_turn": CURRENT_TURN})
     
+    if request.method == 'DELETE':
+        PLAYER_SHAPE = ""
+        BOT_SHAPE = ""
+        CURRENT_TURN = ""
+        return jsonify({"player_shape": PLAYER_SHAPE, "bot_shape": BOT_SHAPE, "current_turn": CURRENT_TURN})
+        
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
